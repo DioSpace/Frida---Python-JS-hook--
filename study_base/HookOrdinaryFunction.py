9b3a5146d60d0843927428c852ee5895c0d6ab33
Dio <zhendong2011@live.cn>
2020-11-24 19:41:11 +0800

    改变下目录结构
    把之前写的一些学习笔记和工作中的一些工具整理到一起

--- study_base/HookOrdinaryFunction.py
+++ study_base/HookOrdinaryFunction.py
@@ -14,9 +14,11 @@ if(Java.available){
     Java.perform(function(){
         var util = Java.use("com.my.fridademo.Util");//获取到类
         util.ordinaryFunc.implementation = function(param1, param2, param3){
+            console.log("Hook Start...");
             //打印堆栈
             console.log(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new()));
-            console.log("Hook Start...");
+            //var stack_print = Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Exception").$new());
+            //console.log(stack_print);
     		send(arguments[0]);
             send(arguments[1]);
             send(arguments[2]);
---
+++ tools/HashMapHandle.py
@@ -0,0 +1,51 @@
+# -*- coding: UTF-8 -*-
+import frida, sys
+
+jsCode = """
+Java.perform(function () {
+    /*
+    var ShufferMap = Java.use('com.xiaojianbang.app.ShufferMap');
+    ShufferMap.show.implementation = function (map) {
+        var result = "";
+        //打印Map
+        var keyset = map.keySet();
+        var it = keyset.iterator();
+        while(it.hasNext()){
+            var keystr = it.next().toString();
+            var valuestr = map.get(keystr).toString();
+            result += valuestr;
+        }
+        send(result);
+        return this.show(map);
+    }
+    */
+    var HashMap = Java.use('java.util.HashMap');
+    var ShufferMap = Java.use('com.xiaojianbang.app.ShufferMap');
+    ShufferMap.show.implementation = function (map) {
+        //初始化一个HashMap并返回
+        var hm = HashMap.$new();
+        hm.put("user","dajianbang");
+        hm.put("pass","87654321");
+        hm.put("code","123456");
+        return this.show(hm);
+    }
+});
+"""
+
+
+def message(message, data):
+    if message["type"] == 'send':
+        print(u"[*] {0}".format(message['payload']))
+    else:
+        print(message)
+
+
+process = frida.get_remote_device().attach("com.xiaojianbang.app")
+
+script = process.create_script(jsCode)
+
+script.on("message", message)
+
+script.load()
+
+sys.stdin.read()
